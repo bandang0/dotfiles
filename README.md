2a1fd47a299037dde8417907fe2329f158a59ce4
naganori-san <g.duque@protonmail.ch>
2017-10-19 02:09:14 +0200

    fix irssi connection

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # dotfiles
 
-Configuration files for Gentoo Linux and Arch Linux (or other Linux).
+Configuration files for Arch Linux (or other Linux distribution using irssi and rxvt-unicode in i3wm).
 
 Usage:
 
-`./install.sh $DISTRO`
-
-where `$DISTRO` is either gentoo_linux or arch_linux.
+`./install.sh`
--- install.sh
+++ install.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-if [ $# -lt 1 ] || [ $# -gt 2 ]; then
-  echo "Script requires exactly one argument: gentoo_linux or arch_linux."
-  exit 1
-fi
-
-for file in $(ls $1); do
-  if [ -d "${1}/${file}" ]; then
+for file in $(ls "archlinux"); do
+  if [ -d "archlinux/${file}" ]; then
     echo -e "Checking if directory /home/$USER/.${file} exists and creating it if not...\n"
     mkdir -pv /home/$USER/.${file}
-    echo -e "Copying files in ${1}/${file} to /home/$USER/.${file}\n"
-    cp -r ${1}/${file}/* /home/$USER/.${file}
+    echo -e "Copying files in archlinux/${file} to /home/$USER/.${file}\n"
+    cp -r archlinux/${file}/* /home/$USER/.${file}
   else
-    echo -e "Copying ${1}/${file} to /home/$USER/.${file}\n"
-    cp ${1}/${file} /home/$USER/.${file}
+    echo -e "Copying archlinux/${file} to /home/$USER/.${file}\n"
+    cp archlinux/${file} /home/$USER/.${file}
   fi
 done
 
 ln -s ~/.irssi/scripts/smartfilter.pl ~/.irssi/scripts/autorun/smartfilter.pl
 
-if [ $# -eq 2 ]; then
-  unzip ~/.irssi/config.zip
-else
-  rm -rf ~/.irssi/config.zip
-fi
-
-echo -e "All done.\n"
+echo -e "All done.\nDon't forget to set your password in the irssi config file.\n"
 exit 0
