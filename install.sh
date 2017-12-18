a877d72f04c4241582dab3e2a30a981f912ac310
bandang0 <bandang0@yahoo.com>
2017-12-18 17:34:51 +0100

    Link config files!

--- install.sh
+++ install.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-#Copy config files
-for file in $(ls "fedora"); do
-  if [ -d "fedora/${file}" ]; then
-    echo -e "Checking if directory /home/$USER/.${file} exists and creating it if not...\n"
-    mkdir -pv /home/$USER/.${file}
-    echo -e "Copying files in fedora/${file} to /home/$USER/.${file}\n"
-    cp -vr fedora/${file}/* /home/$USER/.${file}/
-  else
-    echo -e "Copying fedora/${file} to /home/$USER/.${file}\n"
-    cp fedora/${file} /home/$USER/.${file}
-  fi
+DNAME=fedora
+
+#Link config files
+for file in $(ls $DNAME)
+do
+    echo Linking ~/.$file to ~/dotfiles/$DNAME/$file.
+    rm -rf ~/.$file
+    ln -s ~/dotfiles/$DNAME/$file ~/.$file
 done
 
 #Take care of telegram
 curl https://telegram.org/dl/desktop/linux > ~/Downloads/tsetup.tar.xz
 if [ -s ~/Downloads/tsetup.tar.xz ]
 then
-   tar xvf ~/Downloads/tsetup.tar.xz ~/Downloads/Telegram
-   cp ~/Downloads/Telegram/Telegram ~/.bin/telegram
-   rm -rf ~/Downloads/Telegram
-   rm -rf ~/Downloads/tetup.tar.xz
+    tar xvf ~/Downloads/tsetup.tar.xz ~/Downloads/Telegram
+    cp ~/Downloads/Telegram/Telegram ~/.bin/telegram
+    rm -rf ~/Downloads/Telegram
+    rm -rf ~/Downloads/tetup.tar.xz
 else
     echo Telegram setup file not found, skipping.
 fi
@@ -28,4 +25,4 @@ fi
 #Make directory for Wallpapers
 mkdir -pv ~/Pictures/Wallpapers
 
-echo -e "All done.\nDon't forget to set your password in the irssi config file.\n"
+echo All done.
---
+++ installpython.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Install the usefull python stuff.
+
+pip install --user howdoi tldr astropy
