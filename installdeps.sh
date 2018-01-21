8d950c70114f627b49edda4fdf96945790538d0b
bandang0 <bandang0@yahoo.com>
2018-01-21 23:23:47 +0100

    Add dropbox deamons!

--- installdeps.sh
+++ installdeps.sh
@@ -4,5 +4,5 @@
 #install script.
 
 dnf -y copr enable wyvie/i3blocks
-dnf -y install python2-pip python3-pip python-imaging i3 i3blocks sysstat acpi feh scrot ImageMagick network-manager-applet irssi rxvt-unicode 
+dnf -y install python2-pip python3-pip python-imaging i3 i3blocks sysstat acpi feh scrot ImageMagick network-manager-applet irssi rxvt-unicode p7zip dropbox 
 
