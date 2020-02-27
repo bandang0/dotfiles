cf957e9e1f8a00fb4461f1d1bac4c9c74fe4f43a
Raphaël Duque <bandang0@yahoo.com>
2020-02-27 18:15:32 +0100

    Cleaned-up the dotfiles.
    Signed-off-by: Raphaël Duque <bandang0@yahoo.com>

--- installdeps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Execute this with root proivileges before running the
-#install script.
-
-dnf -y copr enable wyvie/i3blocks
-dnf -y install python2-pip python3-pip python-imaging i3 i3blocks sysstat acpi feh scrot ImageMagick network-manager-applet irssi rxvt-unicode p7zip dropbox
-
--- installpython.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#Install the usefull python stuff.
-
-pip3 install --user howdoi tldr astropy matplotlib
